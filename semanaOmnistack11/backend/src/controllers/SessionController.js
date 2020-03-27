26d0ce0b2966c0069e068e6553f51bb7a83a223b
santanagilberto@uol.com.br <santanagilberto@uol.com.br>
2020-03-27 04:15:54 -0300

    Front-end do app

--- semanaOmnistack11/backend/src/controllers/SessionController.js
+++ semanaOmnistack11/backend/src/controllers/SessionController.js
@@ -1,7 +1,8 @@
 const connection = require('../database/connection');
+
 module.exports={
     async create(request,response){
-        const{id}=request.body;
+        const{ id }=request.body;
         const ong= await connection('ongs')
         .where('id',id)
         .select('name')
